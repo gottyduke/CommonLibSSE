74eeaf1cd272d4430f75dfec27182deaf0943574
max-su-2019 <1336777643@qq.com>
2025-02-03 22:17:39 +0800

    Fixed MovementControllerNPC  offset

--- include/RE/M/MovementControllerAI.h
+++ include/RE/M/MovementControllerAI.h
@@ -66,7 +66,6 @@ namespace RE
 	static_assert(sizeof(MovementArbiter) == 0x18);
 
 	class MovementControllerAI :
-		public BSIntrusiveRefCounted,                // 008
 		public IMovementControllerRegisterInterface  // 000
 	{
 		template <typename T>
@@ -137,36 +136,37 @@ namespace RE
 		virtual void Unk_09(void);                                                   // 09 - { return 1; }
 
 		// members
-		std::uint32_t                           pad00C;    // 00C
-		BSTSmallArray<ActiveArbiterSmartPtr, 2> arbiters;  // 10
-		BSTSmallArray<ActiveAgentSmartPtr, 1>   agents;    // 30
-		std::uint64_t                           unk048;    // 048
-		std::uint64_t                           unk050;    // 050
-		std::uint64_t                           unk058;    // 058
-		std::uint64_t                           unk060;    // 060
-		std::uint64_t                           unk068;    // 068
-		std::uint64_t                           unk070;    // 070
-		std::uint64_t                           unk078;    // 078
-		std::uint64_t                           unk080;    // 080
-		std::uint64_t                           unk088;    // 088
-		std::uint64_t                           unk090;    // 090
-		std::uint64_t                           unk098;    // 098
-		std::uint64_t                           unk0A0;    // 0A0
-		std::uint64_t                           unk0A8;    // 0A8
-		std::uint64_t                           unk0B0;    // 0B0
-		std::uint64_t                           unk0B8;    // 0B8
-		std::uint64_t                           unk0C0;    // 0C0
-		std::uint64_t                           unk0C8;    // 0C8
-		std::uint64_t                           unk0D0;    // 0D0
-		std::uint64_t                           unk0D8;    // 0D8
-		std::uint64_t                           unk0E0;    // 0E0
-		std::uint64_t                           unk0E8;    // 0E8
-		std::uint64_t                           unk0F0;    // 0F0
-		std::uint64_t                           unk0F8;    // 0F8
-		std::uint64_t                           unk100;    // 100
-		std::uint64_t                           unk108;    // 108
-		std::uint64_t                           unk110;    // 110
-		std::uint64_t                           unk118;    // 118
+		mutable volatile std::uint32_t          _refCount{ 0 };  // 008
+		std::uint32_t                           pad00C;          // 00C
+		BSTSmallArray<ActiveArbiterSmartPtr, 2> arbiters;        // 010
+		BSTSmallArray<ActiveAgentSmartPtr, 1>   agents;          // 030
+		std::uint64_t                           unk048;          // 048
+		std::uint64_t                           unk050;          // 050
+		std::uint64_t                           unk058;          // 058
+		std::uint64_t                           unk060;          // 060
+		std::uint64_t                           unk068;          // 068
+		std::uint64_t                           unk070;          // 070
+		std::uint64_t                           unk078;          // 078
+		std::uint64_t                           unk080;          // 080
+		std::uint64_t                           unk088;          // 088
+		std::uint64_t                           unk090;          // 090
+		std::uint64_t                           unk098;          // 098
+		std::uint64_t                           unk0A0;          // 0A0
+		std::uint64_t                           unk0A8;          // 0A8
+		std::uint64_t                           unk0B0;          // 0B0
+		std::uint64_t                           unk0B8;          // 0B8
+		std::uint64_t                           unk0C0;          // 0C0
+		std::uint64_t                           unk0C8;          // 0C8
+		std::uint64_t                           unk0D0;          // 0D0
+		std::uint64_t                           unk0D8;          // 0D8
+		std::uint64_t                           unk0E0;          // 0E0
+		std::uint64_t                           unk0E8;          // 0E8
+		std::uint64_t                           unk0F0;          // 0F0
+		std::uint64_t                           unk0F8;          // 0F8
+		std::uint64_t                           unk100;          // 100
+		std::uint64_t                           unk108;          // 108
+		std::uint64_t                           unk110;          // 110
+		std::uint64_t                           unk118;          // 118
 	};
 	static_assert(sizeof(MovementControllerAI) == 0x120);
 }
